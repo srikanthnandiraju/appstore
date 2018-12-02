35de9e3d0d511b32a51444db33f0ae610a90cfed
Srikanth Nandiraju <srikanthnandiraju@Srikanths-iMac-Pro.local>
2018-12-02 11:57:46 -0800

    UI

---
+++ touchtelugunews/settings.js
@@ -0,0 +1,56 @@
+App.settings = {
+    version: 0,
+    topics: [{
+            name: "Politics",
+            search_text: "Hyderabad Politics"
+        },
+        {
+            name: "Arts & Lifestyle",
+            search_text: "Hyderabad Arts and Lifestyle"
+        },
+        {
+            name: "Kids & family",
+            search_text: "Hyderabad kids activity entertainment"
+        },
+        {
+            name: "Education",
+            search_text: "Hyderabad education"
+        },
+        {
+            name: "Food & drink",
+            search_text: "hyderabad food and drink"
+        },
+        {
+            name: "Games & Fun",
+            search_text: "hyderabad games and fun"
+        },
+        {
+            name: "Health & Fitness",
+            search_text: "hyderabad Health & Fitness"
+        },
+        {
+            name: "Music",
+            search_text: "hyderabad Music and audio"
+        },
+        {
+            name: "Shopping",
+            search_text: "hyderabad shopping"
+        },
+        {
+            name: "Sports",
+            search_text: "hyderabad sports"
+        },
+        {
+            name: "Music",
+            search_text: "hyderabad Music and audio"
+        },
+        {
+            name: "Travel",
+            search_text: "hyderabad travel and transportation"
+        },
+        {
+            name: "Weather",
+            search_text: "hyderabad Weather"
+        }
+    ]
+};
