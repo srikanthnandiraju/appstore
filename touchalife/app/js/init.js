0ab50449d419255cabe547c6192b46dc70dde275
Srikanth Nandiraju <nandiraju@yahoo.com>
2018-12-14 00:01:51 -0800

    agenda added

--- touchalife/app/js/init.js
+++ touchalife/app/js/init.js
@@ -1,37 +1,21 @@
 //1. load navigation for the app.
-$("#navigation-links").load(App.base_url + "/screens/navigation.html");
+//$("#navigation-links").load(App.base_url + "/screens/navigation.html");
 
 // 2. Configure the routes.
-Path.map("#/scavenger_clues").to(function() {
-    $("#title-text").html("Scavenger Hunt");
-    $("#stage").load(App.base_url + "/screens/scavenger_clues.html");
+Path.map("#/home").to(function () {
+    // $("#title-text").html("Scavenger Hunt");
+    $("#stage").load(App.base_url + "/screens/home.html");
 });
-
-Path.map("#/social_feed").to(function() {
-    $("#title-text").html("Feed");
-    $("#stage").load(App.base_url + "/screens/social_feed.html");
-});
-
-Path.map("#/points").to(function() {
-    $("#title-text").html("WOW Points");
-    $("#stage").load(App.base_url + "/screens/points.html");
-});
-
-Path.map("#/logout").to(function() {
-    App.Events.emit('user-logged-out');
-});
-
-
-Path.root("#/scavenger_clues");
+Path.root("#/home");
 Path.listen();
 
 // 3. Attach events to parent.
 // Need this to handle event on generated links
-$('#navigation-links').on('click', '.item', function() {
-    App.Events.emit("hide-side-nav");
-});
+// $('#navigation-links').on('click', '.item', function () {
+//     App.Events.emit("hide-side-nav");
+// });
 
-App.Events.listen("show-announcement-screen", function() {
-    var ref = window.open(App.serverURL + "/content/announcements.html", '_blank',
-        'location=no,footer=yes,closebuttoncaption=Close Announcements,zoom=no,footercolor=#199DDC,closebuttoncolor=#ffffff');
-});
+// App.Events.listen("show-announcement-screen", function () {
+//     var ref = window.open(App.serverURL + "/content/announcements.html", '_blank',
+//         'location=no,footer=yes,closebuttoncaption=Close Announcements,zoom=no,footercolor=#199DDC,closebuttoncolor=#ffffff');
+// });
