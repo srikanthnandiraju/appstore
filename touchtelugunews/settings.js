087d5d2bae2b9b8c5b2f14a972231ad50cc0d2a0
Srikanth Nandiraju <nandiraju@yahoo.com>
2019-02-08 08:44:55 -0800

    app settings

--- touchtelugunews/settings.js
+++ touchtelugunews/settings.js
@@ -1,59 +1,69 @@
 App.settings = {
     version: 0,
     topics: [{
-            name: "Headlines",
-            search_text: "Hyderabad latest news"
-        }, {
             name: "Politics",
-            search_text: "Hyderabad Politics"
+            search_text: "Hyderabad Politics",
+            status: "checked"
         },
         {
             name: "Arts & Lifestyle",
-            search_text: "Hyderabad Arts and Lifestyle"
+            search_text: "Hyderabad Arts and Lifestyle",
+            status: "checked"
         },
         {
             name: "Kids & family",
-            search_text: "Hyderabad kids activity entertainment"
+            search_text: "Hyderabad kids activity entertainment",
+            status: "checked"
         },
         {
             name: "Education",
-            search_text: "Hyderabad education"
+            search_text: "Hyderabad education",
+            status: "checked"
         },
         {
             name: "Food & drink",
-            search_text: "hyderabad food and drink"
+            search_text: "hyderabad food and drink",
+            status: "checked"
         },
         {
             name: "Games & Fun",
-            search_text: "hyderabad games and fun"
+            search_text: "hyderabad games and fun",
+            status: "checked"
         },
         {
             name: "Health & Fitness",
-            search_text: "hyderabad Health & Fitness"
+            search_text: "hyderabad Health & Fitness",
+            status: "checked"
         },
         {
             name: "Music",
-            search_text: "hyderabad Music and audio"
+            search_text: "hyderabad Music and audio",
+            status: "checked"
         },
         {
             name: "Shopping",
-            search_text: "hyderabad shopping"
+            search_text: "hyderabad shopping",
+            status: "checked"
         },
         {
             name: "Sports",
-            search_text: "hyderabad sports"
+            search_text: "hyderabad sports",
+            status: "checked"
         },
         {
             name: "Music",
-            search_text: "hyderabad Music and audio"
+            search_text: "hyderabad Music and audio",
+            status: "checked"
         },
         {
             name: "Travel",
-            search_text: "hyderabad travel and transportation"
+            search_text: "hyderabad travel and transportation",
+            status: "checked"
         },
         {
             name: "Weather",
-            search_text: "hyderabad Weather"
+            search_text: "hyderabad Weather",
+            status: "checked"
         }
     ]
 };
